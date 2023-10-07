bc9e663633ce7f012b389ad8e5573e91df56a122
Anna Carvalho <ajcarvalh01201@gmail.com>
2023-10-07 16:18:40 -0400

    now supports multiple problems

--- plnq.py
+++ plnq.py
@@ -59,9 +59,11 @@ print(f"Making quiz question from {description_file_name}")
 print(f"Placing output in {output_dir_name}")
 print(f"Template files located {template_dir_name}")
 
+
 #
 # Load description
 #
+
 f = open(description_file_name, "r")
 description_json = json.load(f)
 
@@ -78,6 +80,7 @@ exec("".join(data_block), description_globals, description)
 
 # print("Description:")
 # print(description)
+
 
 ##########################################################
 #
@@ -121,6 +124,7 @@ Path(output_dir_name).mkdir(parents=False, exist_ok=False)
 #
 # info.json 
 #
+
 info_json_file = open(f"{template_dir_name}/info.json")
 info_json = json.load(info_json_file)
 info_json["uuid"] = str(uuid.uuid4())
@@ -134,9 +138,11 @@ output_json_file = open(f"{output_dir_name}/info.json", "w")
 json.dump(info_json, output_json_file, indent=2)
 output_json_file.close()
 
+
 #
 # question.html
 #
+
 question_file = open(f"{template_dir_name}/question.html", "r")
 question_file_contents = question_file.read()
 new_question_file_contents = question_file_contents.replace('zzDESCRIPTIONzz', description["info"]["title"])
@@ -145,24 +151,28 @@ output_question_file = open(f"{output_dir_name}/question.html", "w")
 output_question_file.write(new_question_file_contents)
 output_question_file.close()
 
+
 #
 # server.py
 #
-func_name = description["exported_function"]["name"]
-func_desc = description["exported_function"]["description"]
+
+# name/description set to first problem (temporary)
+func_name = description["exported_function"][0]["name"]
+func_desc = description["exported_function"][0]["description"]
 
 server_file = open(f"{template_dir_name}/server.py", "r")
 server_file_contents = server_file.read()
 
-f_name = description['exported_function']['name']
+f_name = description['exported_function'][0]['name']
 update1 = server_file_contents.replace('zzFUNC_NAMEzz',f_name)
 
-f_description = description['exported_function']['description']
+f_description = description['exported_function'][0]['description']
 update2 = update1.replace('zzFUNC_DESCzz', f_description)
 
 output_server_file = open(f"{output_dir_name}/server.py", "w")
 output_server_file.write(update2)
 output_question_file.close()
+
 
 #
 # workspace
@@ -176,56 +186,74 @@ learning_target = json.load(lt_file)
 
 #TODO Verify that this is markdown
 
-title_line = "" # ("# " + description["info"]["title"] + "\n\n")
-text_block = [title_line] + description_json['cells'][1]['source']
+i = 0
+for function in description['exported_function']:
+    func_name = function['name']
+    title_line = "" # ("# " + description["info"]["title"] + "\n\n")
+    text_block = [title_line] + description_json['cells'][i+1]['source']
 
-# find method signature and extract
-methods = []
-for index, line in enumerate(text_block):
-   matches = re.findall("!!!`([^`]+)`!!!", line)
-   methods += matches
-   if (len(matches) > 0):
-       text_block[index] = re.sub("!!!", "", line)
+    # find method signature and extract
+    methods = []
+    for index, line in enumerate(text_block):
+        matches = re.findall("!!!`([^`]+)`!!!", line)
+        methods += matches
+        if (len(matches) > 0):
+            text_block[index] = re.sub("!!!", "", line)
 
-if len(methods) > 1:
-    print("ERROR: Found more than one marked method signature:", methods)
-elif len(methods) == 0:
-    print("WARNING: Did not find a method signature. Using a generic 'your_function'")
-    methods = ["your_function()"]
+    if len(methods) > 1:
+        print("ERROR: Found more than one marked method signature:", methods)
+    elif len(methods) == 0:
+        print("WARNING: Did not find a method signature. Using a generic 'your_function'")
+        methods = ["your_function()"]
 
-method_signature = methods[0]
-possible_names = re.findall("^([^(]+)\(", method_signature)
-if (len(possible_names) != 1):
-    print(f"ERROR: Expected exactly 1 name in {method_signature}.  Found {possible_names}")
-method_name = possible_names[0]
+    method_signature = methods[0]
+    possible_names = re.findall("^([^(]+)\(", method_signature)
+    if (len(possible_names) != 1):
+        print(f"ERROR: Expected exactly 1 name in {method_signature}.  Found {possible_names}")
+    method_name = possible_names[0]
 
-if 'displayed_examples' in description and len(description['displayed_examples']) > 0:
-  text_block += ["\n"]
-  text_block += ["For example:\n"]
-  for example in description['displayed_examples']:
-      num_params = len(example) - 1
-      params = json.dumps(example[:num_params])[1:-1]
-      params = params.replace("true,", "True,")
-      params = params.replace("false,", "False,")
-      answer = example[num_params]
-      text_block += [f"  * `{method_name}({params})` should return `{answer}`\n"] 
+    if 'displayed_examples' in description and func_name in description['displayed_examples'] and len(description['displayed_examples'][func_name]) > 0:
+        text_block += ["\n"]
+        text_block += ["For example:\n"]
+        for example in description['displayed_examples'][func_name]:
+            num_params = len(example) - 1
+            params = json.dumps(example[:num_params])[1:-1]
+            params = params.replace("true,", "True,")
+            params = params.replace("false,", "False,")
+            answer = example[num_params]
+            text_block += [f"  * `{method_name}({params})` should return `{answer}`\n"] 
 
+    text_cell = {
+        "cell_type": "markdown",
+        "metadata": {"collapsed": False},
+        "source": text_block
+    }
 
-learning_target['cells'][0]['source'] = text_block
+    source_text =  ["#grade IMPORTANT: Do not remove or modify this line\n"]
+    source_text += [f'def {methods[0]}:\n']
+    source_text += ['    pass']
 
-source_text =  ["#grade IMPORTANT: Do not remove or modify this line\n"]
-source_text += [f'def {methods[0]}:\n']
-source_text += ['    pass']
+    code_cell = {
+        "cell_type": "code",
+        "execution_count": None,
+        "metadata": {"collapsed": False},
+        "outputs": [],
+        "source": source_text
+    }
+    
+    learning_target['cells'].extend([text_cell, code_cell])
 
-learning_target['cells'][1]['source'] = source_text
+    i += 2
 
 output_lt_file = open(f"{output_dir_name}/workspace/learning_target.ipynb", "w")
 json.dump(learning_target, output_lt_file, indent=2)
 output_lt_file.close()
 
+
 #
 # tests
 #
+
 Path(f"{output_dir_name}/tests").mkdir(parents=False, exist_ok=False)
 shutil.copy(f"{template_dir_name}/tests/ans.py", f"{output_dir_name}/tests")
 shutil.copy(f"{template_dir_name}/tests/initial_code.py", f"{output_dir_name}/tests")
@@ -236,21 +264,25 @@ test_code = test_code_template_file.read()
 test_code += '\n'  # Blank line in case the we loose the newline at the end of the template test.py
 # test_code += f"\n  student_code_file = 'learning_target.ipynb'\n\n"
 
-all_tests = description['displayed_examples'] + description['test_cases']
+i = 0
+for function in description['exported_function']:
+    func_name = function['name']
+    all_tests = description['displayed_examples'][func_name] + description['test_cases'][func_name]
 
+    for test in enumerate(all_tests):
+        num_params = len(test) - 1
+        expected = test[num_params]
+        if isinstance(expected, str):
+            expected = f"'{expected}'"
+        params = json.dumps(test[:num_params])
 
-for index, test in enumerate(all_tests):
-    num_params = len(test) - 1
-    expected = test[num_params]
-    if isinstance(expected, str):
-        expected = f"'{expected}'"
-    params = json.dumps(test[:num_params])
+        test_code += f'  @points(1)\n'
+        test_code += f'  @name("test {i + 1}")\n'
+        test_code += f'  def test_{(i + 1):02d}(self):\n'  # Using a leading 0 ensures they run in numeric order 
+        test_code += f"      self.verify('{func_name}', {expected}, '{params}')\n"
+        test_code += '\n'
 
-    test_code += f'  @points(1)\n'
-    test_code += f'  @name("test {index + 1}")\n'
-    test_code += f'  def test_{(index + 1):02d}(self):\n'  # Using a leading 0 ensures they run in numeric order 
-    test_code += f"      self.verify('{f_name}', {expected}, '{params}')\n"
-    test_code += '\n'
+        i += 1
 
 output_test_file = open(f"{output_dir_name}/tests/test.py", "w")
 output_test_file.write(test_code)
@@ -261,18 +293,26 @@ output_test_file.write(test_code)
 #
 # Make sure the reference solution produces the same answer as the test cases!
 
-answer_block = description_json['cells'][2]['source']
-answer_globals = {}
-answer = {}
-exec("".join(answer_block), answer_globals, answer)
+i = 2
+for function in description['exported_function']:
+    answer_block = description_json['cells'][i]['source']
+    answer_globals = {}
+    answer = {}
+    exec("".join(answer_block), answer_globals, answer)
 
-#TODO: Check for 3rd block.
-# Make sure it is a code block.
-# Make sure it contains a correctly-named function.
+    #TODO: Check for 3rd block.
+    # Make sure it is a code block.
+    # Make sure it contains a correctly-named function.
 
-for index, test in enumerate(all_tests):
-    num_params = len(test) - 1
-    given_answer = test[num_params]
-    computed_answer = answer[f_name](*test[:num_params])
-    if (given_answer != computed_answer):
-        print(f"!!! Test {index}: Given answer {given_answer} differs from computed answer {computed_answer}")
+    func_name = function['name']
+    all_tests = description['displayed_examples'][func_name] + description['test_cases'][func_name]
+
+    for index, test in enumerate(all_tests):
+        num_params = len(test) - 1
+        given_answer = test[num_params]
+        computed_answer = answer[func_name](*test[:num_params])
+        
+        if (given_answer != computed_answer):
+            print(f"!!! Test {index}: Given answer {given_answer} differs from computed answer {computed_answer}")
+
+    i += 2
