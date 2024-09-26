53e8b031da1146dba871790c3a9a5a24051b73fe
Zachary Kurmas <kurmasz@gvsu.edu>
2024-09-26 17:43:46 -0400

    Tests set up

--- test/unit_tests/test_helpers.py
+++ test/unit_tests/test_helpers.py
@@ -10,8 +10,9 @@
 
 import sys
 import unittest
+import os
 
-sys.path.append('../..')
+sys.path.append(os.path.dirname(__file__) + '/../..')
 import plnq
 
 class HelperTests(unittest.TestCase):
@@ -22,31 +23,31 @@ class HelperTests(unittest.TestCase):
 
     def test_daor_single_component(self):
         observed = plnq.determine_absolute_output_dir("the_dir", "the_input.ipynb")
-        self.assertEquals("the_dir", observed)
+        self.assertEqual("the_dir", observed)
 
     def test_daor_relative_path(self):
         observed = plnq.determine_absolute_output_dir("a/b/c/dir", "the_input.ipynb")
-        self.assertEquals("a/b/c/dir", observed)
+        self.assertEqual("a/b/c/dir", observed)
 
     def test_daor_absolute_path(self):
         observed = plnq.determine_absolute_output_dir("/a/b/c/dir", "the_input.ipynb")
-        self.assertEquals("/a/b/c/dir", observed)
+        self.assertEqual("/a/b/c/dir", observed)
 
     def test_daor_relative_with_dash_single_input(self):
         observed = plnq.determine_absolute_output_dir("a/b/c/-", "the_input.ipynb")
-        self.assertEquals("a/b/c/the_input", observed)
+        self.assertEqual("a/b/c/the_input", observed)
 
     def test_daor_relative_with_dash_relative_input(self):
         observed = plnq.determine_absolute_output_dir("a/b/c/-", "g/h/i/j/the_input.ipynb")
-        self.assertEquals("a/b/c/the_input", observed)
+        self.assertEqual("a/b/c/the_input", observed)
 
     def test_daor_relative_with_dash_absolute_input(self):
         observed = plnq.determine_absolute_output_dir("a/b/c/-", "/g/h/i/j/the_input.ipynb")
-        self.assertEquals("a/b/c/the_input", observed)
+        self.assertEqual("a/b/c/the_input", observed)
 
     def test_daor_relative_with_dash_obnoxious_input1(self):
         observed = plnq.determine_absolute_output_dir("a/b/c/-", "g/why.ipynb/i/j/the_input.ipynb")
-        self.assertEquals("a/b/c/the_input", observed)
+        self.assertEqual("a/b/c/the_input", observed)
 
     def test_daor_non_ipynb_raises_exception1(self):
         with self.assertRaises(ValueError):
