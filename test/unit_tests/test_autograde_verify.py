c93740cc79fd648fab1cd1795de861791b05e05a
Zachary Kurmas <kurmasz@gvsu.edu>
2025-06-09 21:20:16 -0400

    End of day check-in

---
+++ test/unit_tests/test_autograde_verify.py
@@ -0,0 +1,119 @@
+###############################################################################
+#
+# test_plnq_test.py
+#
+# Unit tests for Test#verify from the Workbook's test.py
+#
+# (c) 2025 Zachary Kurmas
+#
+###############################################################################
+import unittest
+import os
+import sys
+
+# Since we don't know the cwd of the process that will run this test, we need to
+# dynamically add the path to the generated directory where the mock_pl module is located.
+generated_dir = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', '..', 'mock_pl'))
+sys.path.insert(0, generated_dir)
+
+generated_dir2 = os.path.abspath(os.path.join(os.path.dirname(
+    __file__), '..', '..', 'plnq', 'quiz_template', 'tests'))
+sys.path.insert(0, generated_dir2)
+
+from code_feedback import Feedback
+from test import Test
+
+class StudentCode:
+
+    @staticmethod
+    def two_x_plus_y(x, y):
+        StudentCode.two_x_plus_y_called = True
+        StudentCode.x_parm = x
+        StudentCode.y_parm = y
+        return 2*x + y
+
+    @staticmethod
+    def three_x_minus_y(x, y):
+        StudentCode.three_x_minus_y_called = True
+        StudentCode.x_parm = x
+        StudentCode.y_parm = y
+        return 3*x - y
+
+    @staticmethod
+    def return_hello():
+        StudentCode.return_hello_called = True
+        StudentCode.x_parm = -999
+        StudentCode.y_parm = -777
+        return "Hello"
+
+
+class TestVerifyTest(unittest.TestCase):
+
+    def test_verify_calls_named_function(self):
+        the_test = Test()
+        the_test.st = StudentCode()
+
+        StudentCode.two_x_plus_y_called = False
+        StudentCode.three_x_minus_y_called = False
+        StudentCode.return_hello_called = False
+
+        the_test.verify(
+            function_name='two_x_plus_y',
+            expected=17,
+            params_json='[5, 7]',
+        )
+
+        self.assertTrue(StudentCode.two_x_plus_y_called)
+        self.assertFalse(StudentCode.three_x_minus_y_called)
+        self.assertFalse(StudentCode.return_hello_called)
+
+        self.assertEqual(StudentCode.x_parm, 5)
+        self.assertEqual(StudentCode.y_parm, 7)
+
+    def test_verify_calls_named_function_with_no_params(self):
+        the_test = Test()
+        the_test.st = StudentCode()
+
+        StudentCode.two_x_plus_y_called = False
+        StudentCode.three_x_minus_y_called = False
+        StudentCode.return_hello_called = False
+
+        the_test.verify(
+            function_name='return_hello',
+            expected='hello',
+            params_json='[]',
+        )
+
+        self.assertFalse(StudentCode.two_x_plus_y_called)
+        self.assertFalse(StudentCode.three_x_minus_y_called)
+        self.assertTrue(StudentCode.return_hello_called)
+        self.assertEqual(StudentCode.x_parm, -999)
+        self.assertEqual(StudentCode.y_parm, -777)
+
+    def test_call_cast_if_not_None(self):
+        the_test = Test()
+        the_test.st = StudentCode()
+
+        cast_param = None
+
+        def the_cast(x):
+            nonlocal cast_param
+            cast_param = x
+
+        StudentCode.two_x_plus_y_called = False
+        StudentCode.three_x_minus_y_called = False
+        StudentCode.return_hello_called = False
+
+        the_test.verify(
+            function_name='three_x_minus_y',
+            expected=8,
+            params_json='[5, 7]',
+            cast=the_cast
+        )
+
+        self.assertEqual(cast_param, 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
